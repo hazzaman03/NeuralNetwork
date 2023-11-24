7b162efc0294934eb1c88d8a4743d2d910427d24
Harry Lane <harryslane.03@gmail.com>
2023-11-24 13:00:14 +1100

    made interface

---
+++ interface.py
@@ -0,0 +1,58 @@
+from tkinter import *
+from tkinter.colorchooser import askcolor
+
+
+class Paint(object):
+
+    def __init__(self):
+        self.root = Tk()
+
+        self.reset_button = Button(self.root, text='reset', command=self.clear)
+        self.reset_button.grid(row='1', column='2')
+
+        self.c = Canvas(self.root, bg='black', width=560, height=560)
+        self.c.grid(row='0', columnspan=5)
+        
+        self.setup()
+        self.root.mainloop()
+
+    def setup(self):
+        self.old_x = None
+        self.old_y = None
+        self.line_width = 40
+        self.color = 'white'
+        self.c.bind('<B1-Motion>', self.paint)
+        self.c.bind('<ButtonRelease-1>', self.reset)
+
+    def use_pen(self):
+        self.activate_button(self.pen_button)
+
+    def use_brush(self):
+        self.activate_button(self.brush_button)
+
+    def activate_button(self, some_button, eraser_mode=False):
+        self.active_button.config(relief=RAISED)
+        some_button.config(relief=SUNKEN)
+        self.active_button = some_button
+        self.eraser_on = eraser_mode
+
+    def paint(self, event):
+        if self.old_x and self.old_y:
+            self.c.create_line(self.old_x, self.old_y, event.x, event.y,
+                               width=self.line_width, fill=self.color,
+                               capstyle=ROUND, smooth=TRUE, splinesteps=36)
+        self.old_x = event.x
+        self.old_y = event.y
+
+    def reset(self, event):
+        self.old_x, self.old_y = None, None
+    
+    def clear(self):
+        self.c = Canvas(self.root, bg='black', width=560, height=560)
+        self.c.grid(row='0', columnspan=5)
+        self.c.bind('<B1-Motion>', self.paint)
+        self.c.bind('<ButtonRelease-1>', self.reset)
+
+
+if __name__ == '__main__':
+    Paint()
